667e01484184fdb40c30c02bd008da5eea3c0cde
Sam Zhao <zhao.shengjun@gmail.com>
2017-11-10 09:55:49 +1000

    flatten array

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-function trimAndLower(text: string | null | undefined) {
-  if (typeof text === 'string') {
-    return text.trim().toLowerCase();
+const numbers = [1, 2, [3, 4], 5, [6], [7], 8, 9];
+function flatten(array: (number | number[])[]): number[] {
+  let flattened: number[] = [];
+  for (const el of array) {
+    if (Array.isArray(el)) {
+      flattened.push(...el);
+    } else {
+      flattened.push(el);
+    }
   }
-  return text;
+  return flattened;
 }
-//text will be shown as different type in the if/else blocks.
+
+console.log(flatten(numbers));
