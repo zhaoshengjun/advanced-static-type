f8be6ebbed8d3c343750cc27d0efe6c08f43e042
Sam Zhao <zhao.shengjun@gmail.com>
2017-11-10 09:57:44 +1000

    generic flatten

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const numbers = [1, 2, [3, 4], 5, [6], [7], 8, 9];
-function flatten(array: (number | number[])[]): number[] {
-  let flattened: number[] = [];
+const strings = ['a', 'b', ['c'], ['d', 'e', 'f'], 'g', 'h'];
+function flatten<T>(array: (T | T[])[]): T[] {
+  let flattened: T[] = [];
   for (const el of array) {
     if (Array.isArray(el)) {
       flattened.push(...el);
@@ -12,3 +13,4 @@ function flatten(array: (number | number[])[]): number[] {
 }
 
 console.log(flatten(numbers));
+console.log(flatten(strings));
