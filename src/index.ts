7a082b8acd3e2eb7298f870f2b2b19adf8317e0c
Sam Zhao <zhao.shengjun@gmail.com>
2017-11-10 10:00:46 +1000

    custom type guard function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 const numbers = [1, 2, [3, 4], 5, [6], [7], 8, 9];
 const strings = ['a', 'b', ['c'], ['d', 'e', 'f'], 'g', 'h'];
-function flatten<T>(array: (T | T[])[]): T[] {
-  let flattened: T[] = [];
-  for (const el of array) {
-    if (Array.isArray(el)) {
-      flattened.push(...el);
-    } else {
-      flattened.push(el);
-    }
-  }
-  return flattened;
-}
 
-console.log(flatten(numbers));
-console.log(flatten(strings));
+function isFlat<T>(array: (T | T[])[]): array is T[] {
+  return !array.some(Array.isArray);
+}
+if (isFlat(numbers)) {
+  numbers;
+}
