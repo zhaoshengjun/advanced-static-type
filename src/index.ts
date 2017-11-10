af4ef7f478a3be771c59c931b2bbff10441c0eb0
Sam Zhao <zhao.shengjun@gmail.com>
2017-11-10 10:01:08 +1000

    clean slate

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const numbers = [1, 2, [3, 4], 5, [6], [7], 8, 9];
-const strings = ['a', 'b', ['c'], ['d', 'e', 'f'], 'g', 'h'];
-
-function isFlat<T>(array: (T | T[])[]): array is T[] {
-  return !array.some(Array.isArray);
-}
-if (isFlat(numbers)) {
-  numbers;
-}
